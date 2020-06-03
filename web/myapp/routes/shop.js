6510071d4cd28df3cde2256cc72afdfaf4d6bf2e
rdk <shishov.dmb@yandex.ru>
2020-06-03 17:59:31 +0300

    Started Express Web Server

---
+++ web/myapp/routes/shop.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.resolve(__dirname, '../public/shop.html'));
+
+});
+
+module.exports = router;
